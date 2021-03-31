0e83995ba65a8a340010e125c53be74eee49dd2b
Eshark-YT <81536250+Eshark-YT@users.noreply.github.com>
2021-03-31 18:07:38 +0100

    Update Eshark.YT-download.py
    Updated download options

--- Eshark.YT-download.py
+++ Eshark.YT-download.py
@@ -3,13 +3,16 @@ from enum import Enum
 from termcolor import colored
 
 # SELECT VIDEO FOR YT
-Video = input("What youtube video do you want to download: \n")
+Video = input("What video do you want to download: \n")
 print(colored("Video selected:", 'green'), Video)
 
 # SELECT FORMAT FOR DOWNLOAD
 format = "Select Format:", "1. m4a (audio only)", "2. mp4_144p", "3. mp4_240p", "4. mp4_360p ", "5. mp4_480p ", "6. mp4_720p ", "7. mp4_1080p", "8. gp3_176_144", "9. gp3_320_240", "10. flv", "11. webm", "12. mp4_640_360", "13. mp4_1280_720 "
 print(*format, sep="\n")
 format_options = input("Select Format: ")
+
+if format_options != 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    print("fail")
 
 
 class YLFormat(Enum):
@@ -29,32 +32,67 @@ class YLFormat(Enum):
 
 
 ylformat = {}
+
 if format_options == "1":
     ylformat = YLFormat.m4a
     print(colored("Selected format:", 'red'), "m4a (audio only)")
+
 elif format_options == "2":
     ylformat = YLFormat.mp4_144p
+    print(colored("Selected format:", 'red'), "mp4_144p")
+    
 elif format_options == "3":
-    ylformat = YLFormat.mp4_240p
+      ylformat = YLFormat.mp4_240p
+      print(colored("Selected format:", 'red'), "mp4_240p")
+    
 elif format_options == "4":
-    ylformat = YLFormat.mp4_360p
+       ylformat = YLFormat.mp4_360
+       print(colored("Selected format:", 'red'), "mp4_360p")
+  
 elif format_options == "5":
     ylformat = YLFormat.mp4_480p
+    print(colored("Selected format:", 'red'), "mp4_480p")
+    
 elif format_options == "6":
     ylformat = YLFormat.mp4_720p
+    print(colored("Selected format:", 'red'), "mp4_720p")
+    
 elif format_options == "7":
     ylformat = YLFormat.mp4_1080p
+    print(colored("Selected format:", 'red'), "mp4_1080p")
+    
+elif format_options == "8":
+    ylformat = YLFormat.gp3_176_144
+    print(colored("Selected format:", 'red'), "gp3_176_144")
+    
+elif format_options == "9":
+    ylformat = YLFormat.gp3_320_240
+    print(colored("Selected format:", 'red'), "gp3_320_240")
+    
+elif format_options == "10":
+    ylformat = YLFormat.flv
+    print(colored("Selected format:", 'red'), "Flv")
+    
+elif format_options == "11":
+    ylformat = YLFormat.webm
+    print(colored("Selected format:", 'red'), "webm")
+    
+elif format_options == "12":
+    ylformat = YLFormat.mp4_640_360
+    print(colored("Selected format:", 'red'), "mp4_640_360")
+    
+elif format_options == "13":
+    ylformat = YLFormat.mp4_1280_720
+    print(colored("Selected format:", 'red'), "mp4_1280_720")
+    
 else:
-    print("Please try again, typing the number")
+    print("Invalid input. Please run the script again to try again")
+    wait(1)
+    end()
 
 def download(url: str, options: dict):
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([url])
-
-
-# SELECT FORMAT HERE
-# ylformat = YLFormat.m4a
-print(colored("Selected format:", 'red'), ylformat)
 
 download_list = [
     (Video, ylformat,),
@@ -82,7 +120,9 @@ if format_conformation == "Y":
                 break
             break
         break
-        print("downloaded")
+    print("Finished")
+    exit()
 
-    else:
-        print("Change the format in the source code. Further updated will bw coming soon.")
+else:
+    print("Run this script to try again.")
+    exit()
